badd3fef3e81da90785d8b28417305c516e06209
Josh Ghiloni <ghiloni@gmail.com>
2024-12-27 22:36:32 -0500

    move config.yml

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: "public",
   build: {
     outDir: "image/public",
     rollupOptions: {
@@ -14,8 +15,20 @@ export default defineConfig({
           "react-dom": "ReactDOM",
         },
         manualChunks(id) {
+          console.log(id);
+          if (id.includes(".map")) {
+            console.warn("*** HEY HEY HEY ***")
+          }
           if (/projectEnv.ts/.test(id)) {
             return "projectEnv";
+          }
+
+          if (id.includes("node_modules/decap-cms")) {
+            return "decap-cms";
+          }
+
+          if (id.includes("node_modules")) {
+            return "vendor";
           }
         },
       },
